31b9f6973dd5835b62fd7da7379e1adfd5ae3270
JacquelynSH <elpatchaembroidery@gmail.com>
2023-01-23 10:03:37 -0800

    setup state/setup multiple returns/add a setTimeOut

--- src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
+++ src/tutorial/3-conditional-rendering/setup/1-multiple-returns.js
@@ -1,7 +1,43 @@
 import React, { useState, useEffect } from 'react';
 const url = 'https://api.github.com/users/QuincyLarson';
 const MultipleReturns = () => {
-  return <h2>multiple returns</h2>;
+  const [isloading, setIsLoading] = useState(true);
+  const [isError, setIsError] = useState(false);
+  const [user, setUser] = useState('default user');
+
+  useEffect(() => {
+    setIsLoading(true);
+    setTimeout(() => {
+      fetch(url)
+        .then((resp) => resp.json())
+        .then((user) => {
+          const { login } = user;
+          setUser(login);
+          setIsLoading(false);
+        })
+        .catch((error) => console.log(error));
+    }, 3000);
+  }, [])
+
+  if (isloading) {
+    return (
+      <div>
+        <h2>Loading....</h2>
+      </div>
+    );
+  }
+  if (isError) {
+    return (
+      <div>
+        <h1>Error.....</h1>
+      </div>
+    );
+  }
+  return (
+    <div>
+      <h2>{user}</h2>
+    </div>
+  )
 };
 
 export default MultipleReturns;
